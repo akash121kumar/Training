bfa9ac925407fcf0d3aa398a1358c5eb7f5109e4
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-09 11:51:47 +0530

    Swapping of two numbers using parallel assignments and type_casting

--- Ruby/parallel_assignments.rb
+++ Ruby/parallel_assignments.rb
@@ -1,2 +1,9 @@
-a,b,c= 10,30,40
-puts a,b,c
+puts "Enter first number"
+a=gets.to_i
+
+puts "Enter second number"
+b=gets.to_i
+puts "value before swapping a=#{a} and b=#{b} "
+a,b = b,a
+puts "value after swapping a=#{a} and b=#{b}"
+puts "The sum of a and b is #{a+b}"
