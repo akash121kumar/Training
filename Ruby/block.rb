aff88b08897fcf07f28ebbf6f5043ee73d134c03
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-09 18:38:07 +0530

    Yield with parameter

--- Ruby/block.rb
+++ Ruby/block.rb
@@ -1,10 +1,19 @@
 
+# def test
+# 	puts 'This is inside function before block'
+# 	yield
+# 	puts 'This is inside function after block'
+# 	yield
+# end
+# test{
+# 	puts "This is inside block"
+# }
+
+
+
 def test
-	puts 'This is inside function before block'
-	yield
-	puts 'This is inside function after block'
-	yield
+   yield 5
+   puts "You are in the method test"
+   yield 100
 end
-test{
-	puts "This is inside block"
-}
+test {|i| puts "You are in the block #{i}"}
