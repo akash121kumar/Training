726cde3ca2b31158d473c4d9a4af62c168c15a51
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-09 13:06:36 +0530

    case statement addedq

--- Ruby/conditional_statements.rb
+++ Ruby/conditional_statements.rb
@@ -17,3 +17,18 @@ unless y<5
 else 
 	"Y is greater than 5"
 end
+
+#case statement
+$age =  22
+case $age
+when 0 .. 2
+   puts "baby"
+when 3 .. 6
+   puts "little child"
+when 7 .. 12
+   puts "child"
+when 13 .. 18
+   puts "youth"
+else
+   puts "adult"
+end
