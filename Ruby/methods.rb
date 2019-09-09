1b5f0c8da2af0a4a367100d371c427d4cec9cad5
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-09 17:10:36 +0530

    Methods taking variable/multi arguments

--- Ruby/methods.rb
+++ Ruby/methods.rb
@@ -5,12 +5,23 @@
 # test "C", "C++"
 # test
 
-def test
-	i=10
-	j=50
-	k=20
-	return i,j,k
+# def test
+# 	i=10
+# 	j=50
+# 	k=20
+# 	return i,j,k
+# end
+# var=test
+# puts var
+# puts var.class
+
+
+#method that is taking number of methods 
+def sample (*test)
+   puts "The number of parameters is #{test.length}"
+   for i in 0...test.length
+      puts "The parameters are #{test[i]}"
+   end
 end
-var=test
-puts var
-puts var.class
+sample "Akash", "6", "M","Btech","4years"
+sample "Vikas", "36", "M", "MCA"
