7443ef0be340dbf32f950ce16624bff8511bb16c
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-09 15:24:53 +0530

    Next statement added

--- Ruby/break_statement.rb
+++ Ruby/break_statement.rb
@@ -1,6 +1,15 @@
+# for i in 0..5
+#    if i > 2 then
+#       break
+#    end
+#    puts "Value of local variable is #{i}"
+# end
+
+#ruby next statement>>
+
 for i in 0..5
-   if i > 2 then
-      break
-   end
-   puts "Value of local variable is #{i}"
+	if i>2
+		next
+	end
+	puts "The value of i is #{i}"
 end
