75f808afd3afdd17b776a525729ce6b935540b48
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-09 16:50:42 +0530

    Methods that is returning a value

--- Ruby/methods.rb
+++ Ruby/methods.rb
@@ -1,6 +1,16 @@
-def test(a1 = "Ruby", a2 = "Python")
-   puts "The programming language is #{a1}"
-   puts "The programming language is #{a2}"
+# def test(a1 = "Ruby", a2 = "Python")
+#    puts "The programming language is #{a1}"
+#    puts "The programming language is #{a2}"
+# end
+# test "C", "C++"
+# test
+
+def test
+	i=10
+	j=50
+	k=20
+	return i,j,k
 end
-test "C", "C++"
-test
+var=test
+puts var
+puts var.class
