f7849699f37ff34616773dcf435cd692af9ddeb5
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-09 10:58:35 +0530

    printed the value with keys

--- Ruby/hashes.rb
+++ Ruby/hashes.rb
@@ -1,2 +1,4 @@
-Student={1 => 'Akash',2 => 'Vikas',3 => 'Jai', 4 => 'Veeru'}
-puts Student
+student={1 => 'Akash',2 =>'Vikas',3 => 'Jai',4 => 'Veeru'}
+student.each do|key,value|
+	print "id is ",key," value is ",value ,"\n"
+end
