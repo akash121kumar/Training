0ebf8752f9f3ff49d4341f8a108e2a790fcc3936
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-09 14:35:26 +0530

    Until Statement and Until modifier added

--- Ruby/While_loop.rb
+++ Ruby/While_loop.rb
@@ -1,14 +1,26 @@
 $i=0
 $num=5
-while $i<$num do 
-	puts "Inside loop i=#{$i}"
-	$i+=1
-end
+# while $i<$num do 
+# 	puts "Inside loop i=#{$i}"
+# 	$i+=1
+# end
 
 #while modifier
-j=0
-numb=10
-begin
-	puts "Inside while modifier loop i=#{j}"
-	j+=1
-end while j<numb
+# j=0
+# numb=10
+# begin
+# 	puts "Inside while modifier loop i=#{j}"
+# 	j+=1
+# end while j<numb
+
+#until statement 
+# until $i>$num do
+# 	puts "Inside loop i=#{$i}"
+# 	$i+=1
+# end
+
+#until modifier
+begin 
+	puts "Inside until modifier i=#{$i}"
+	$i+=1
+end until $i>$num
