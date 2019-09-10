be24aae0e669acc20ece38f70129781c401beb21
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-10 18:36:56 +0530

    Hashes and their methods

---
+++ Ruby/hashes_method.rb
@@ -0,0 +1,7 @@
+grades = {'akash'=> 10, 'vikas' => 7}
+# puts grades['akash']
+# grades = Hash.new
+# print grades.class
+# grades["vijay"]= 10
+# print grades
+grades.each{|key,values| puts "key is #{key} and value is #{values}"}
