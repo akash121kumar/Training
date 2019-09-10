cca02ea52630daafb64c7f2a34b85db9dd870973
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-10 12:19:01 +0530

    methods of strings

--- Ruby/strings.rb
+++ Ruby/strings.rb
@@ -1,4 +1,29 @@
-str1="This is my string"
-ss=str1
-puts ss.class
-puts ss
+str1="Hello "
+# ss=str1
+# puts ss.class
+# puts ss
+
+# puts str1 * 3
+str2="Hello "
+puts str1 <=> str2
+
+ puts str1[0]
+ puts str1[0,5]
+ puts str1[0..5]
+ puts str1['el']
+ puts "hello".byteslice(-1)
+ puts str1.capitalize
+ puts str1 .downcase
+ puts str1.upcase
+ puts str1.center(20)
+ puts str1.each_char.to_a
+ puts str1.chr
+ # str1.clear
+ puts str1
+ str2<<"world"
+ puts str2
+ puts str2.count 'l'
+puts str1.delete 'l'
+puts str2.each_byte{|c| print c , ' '}
+str2.each_char{|c| print c , ' '}
+puts str2.empty?
