2c7720c212aa6eb1e2cf00db78b23938a5b82810
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-11 12:53:14 +0530

    Hashes method completed

--- Ruby/hashes_method.rb
+++ Ruby/hashes_method.rb
@@ -43,7 +43,7 @@
 # puts h1["a"]
 # puts h1.compare_by_identity?
 
-# h1={"a" => 100,"b" =>200}
+h1={"a" => 100,"b" =>200,"d" => 500}
 # h1.delete("a")						#Deletes the key value pair
 # print h1, "\n"
 # print h1.delete("z") { |el| "#{el}  not found"}, "\n"
@@ -52,5 +52,35 @@ h = {"a" => 100, "b"=> 200, "c" => 300}
 # h.delete_if {|key, value| key >= "b"}
 # print h,"\n"
 
-h.each_pair{|key,value| puts "#{key} is #{value}"}		#enumerates key and values
-print h.empty? ,"\n"
+# h.each_pair{|key,value| puts "#{key} is #{value}"}		#enumerates key and values
+# print h.empty? ,"\n"
+
+#  print h.eql?(h1) #compares two hashes and gives boolean value
+ # puts h.fetch("a")  #fetch the value of key
+
+# print h.flatten			#converts into array
+# print h.flatten(2)
+
+# print h.has_key?("a")
+# print h.has_value?(300)
+
+# print h.invert				#convert value as a key and key as a value
+# a=h.keys						#returns all the keys of a hash
+# print a ,"\n"
+# b=h.values						#returns all the values from hash
+# print b ,"\n"
+
+# print h.merge(h1), "\n"				#merges two hashes
+
+# print h.rassoc(200) 				#search in values 
+
+# h.replace({"e" => 300 , "d" => 400 })	#replaces eixisting hash with new key_value pairs
+# print h ,"\n"
+
+# h.shift 			#deletes a key value pair from hash
+# print h
+
+# a=h.store("a",100) 		#stores a value 
+# print a.class
+
+print h.to_a				#converts the hash into an array
