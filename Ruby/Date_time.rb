4677fa2650055b86b385caef8c76084b41cb9cfe
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-11 14:01:22 +0530

    Date and time

---
+++ Ruby/Date_time.rb
@@ -0,0 +1,20 @@
+
+#For current time
+# time=Time.new
+# time1=Time.now
+# puts "Current time :" + time1.inspect
+# puts "Current time :" + time.inspect
+
+#Time components
+time=Time.new
+puts "Current Time : "+ time.inspect
+puts time.year 			#Year of the date
+puts time.month 		#Month of the date
+puts time.day			#Day of the date
+puts time.wday			#Day of week
+puts time.yday			#Day of year
+puts time.hour			#gives hour
+puts time.min  			#gives minutes
+puts time.sec			#gives seconds
+puts time.usec			#gives microseconds
+puts time.zone			#gives the timezonde
