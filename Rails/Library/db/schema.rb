1ffc232254e8af6469b1be376f86554abddf1b8e
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-16 14:17:29 +0530

    category table created

--- Rails/Library/db/schema.rb
+++ Rails/Library/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_051925) do
+ActiveRecord::Schema.define(version: 2019_09_16_084457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2019_09_16_051925) do
     t.string "name"
     t.string "author"
     t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "Category"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
