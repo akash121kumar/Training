68099affaf292cb347ed6117088d744a972f42f0
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-16 12:24:05 +0530

    Migration modified for books table

--- Rails/Library/db/migrate/20190916051925_create_books.rb
+++ Rails/Library/db/migrate/20190916051925_create_books.rb
@@ -2,6 +2,10 @@ class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
 
+      t.string :name
+      t.string :author
+      t.integer :price
+
       t.timestamps
     end
   end
