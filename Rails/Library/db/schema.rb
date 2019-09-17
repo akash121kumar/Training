e186a1a98216c7324ec92b3421d6c96f9eac81d2
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-17 10:26:38 +0530

    Student model added

--- Rails/Library/db/schema.rb
+++ Rails/Library/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_044820) do
+ActiveRecord::Schema.define(version: 2019_09_17_045407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2019_09_17_044820) do
 
   create_table "departments", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
