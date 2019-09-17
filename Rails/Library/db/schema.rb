1a65fc79275c37d02d3f17bf263a7fa3410e2620
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-17 10:22:34 +0530

    Added Department model

--- Rails/Library/db/schema.rb
+++ Rails/Library/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_085821) do
+ActiveRecord::Schema.define(version: 2019_09_17_044820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2019_09_16_085821) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
