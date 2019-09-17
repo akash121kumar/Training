df2ae36720ba8c5a376a0ce51a35b662a2421bf8
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-17 10:34:55 +0530

    BookIssue model added

--- Rails/Library/db/schema.rb
+++ Rails/Library/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_045407) do
+ActiveRecord::Schema.define(version: 2019_09_17_045824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "book_issues", force: :cascade do |t|
+    t.date "issue_date"
+    t.date "return_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "name"
