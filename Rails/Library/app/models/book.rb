e423dd9954cff11072d0efd10621c5ef0e2ae98d
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-19 19:17:26 +0530

    Associations Modified

--- Rails/Library/app/models/book.rb
+++ Rails/Library/app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
 	belongs_to :category
 	has_many :book_issues
-	has_one :student, through: :book_issue
+	has_many :students, through: :book_issues
 	validates :name , presence: true
 end
