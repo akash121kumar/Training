d410cfa185b4b347c1d45aed84378c9a76d5adb8
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-23 15:51:47 +0530

    callback added

--- Rails/Library/app/models/student.rb
+++ Rails/Library/app/models/student.rb
@@ -1,9 +1,16 @@
 class Student < ApplicationRecord
+	before_validation :normalize_name, on: :create
 	belongs_to :department
 	has_many :book_issues
 	has_many :books, through: :book_issues
 	validates :name, :address , presence: true
 	validates :name, length: { minimum: 2 }
 	validates :name, length: { maximum: 30}
+	
+
+	private
+	  def normalize_name
+      self.name = name.downcase.titleize
+      end
 
 end
