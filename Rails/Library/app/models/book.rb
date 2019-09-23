dc64a3dfc41aee1cf7b439d9ec306b0a09fe6226
Akash <akashkumar121.ak.ak@gmail.com>
2019-09-23 12:07:19 +0530

    few Validation added

--- Rails/Library/app/models/book.rb
+++ Rails/Library/app/models/book.rb
@@ -3,4 +3,6 @@ class Book < ApplicationRecord
 	has_many :book_issues
 	has_many :students, through: :book_issues
 	validates :name , presence: true
+	validates :name, length: { minimum: 2 }
+	
 end
--- Rails/Library/app/models/student.rb
+++ Rails/Library/app/models/student.rb
@@ -2,9 +2,8 @@ class Student < ApplicationRecord
 	belongs_to :department
 	has_many :book_issues
 	has_many :books, through: :book_issues
-
-
-	
 	validates :name, :address , presence: true
+	validates :name, length: { minimum: 2 }
+	validates :name, length: { maximum: 30}
 
 end
